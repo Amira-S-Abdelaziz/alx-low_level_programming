b8a4619eeebbac050eebb8facae3c5aa07c8cd9c
Amira-S-Abdelaziz <e.amira67@gmail.com>
2023-07-27 21:13:59 +0000

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: zero
+ */
+int main(void)
+{
+	int a;
+
+	for (a = (int)'0' ; a <= (int)'9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
