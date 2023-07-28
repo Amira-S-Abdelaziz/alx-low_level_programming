bdd24bc7552f597d8d1bedc5ac54c07c4daf0ba8
Amira-S-Abdelaziz <e.amira67@gmail.com>
2023-07-28 15:47:20 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *  add - function
+ *  @a: var
+ *  @b: var
+ *  Return: smthing
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
