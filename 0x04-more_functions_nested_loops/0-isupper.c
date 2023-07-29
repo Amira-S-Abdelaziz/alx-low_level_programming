f8599b73bc6ec95c72d584ae1288aabf9b76980d
Amira-S-Abdelaziz <e.amira67@gmail.com>
2023-07-29 17:54:21 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _isupper - function
+ * @c: var
+ * Return: smthng
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
