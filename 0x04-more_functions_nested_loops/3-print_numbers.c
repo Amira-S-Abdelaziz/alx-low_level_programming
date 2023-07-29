ec231741abc32688376498a81c3d0afed52c36a6
Amira-S-Abdelaziz <e.amira67@gmail.com>
2023-07-29 18:12:49 +0000

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_numbers - function
+ * Return: smthng
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = (int)'0'; a <= (int)'9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+void print_numbers(void);
+int _putchar(char c);
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
