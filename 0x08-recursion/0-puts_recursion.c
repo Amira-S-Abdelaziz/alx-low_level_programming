a6d97a7cc1f8899e36426d29d2b55e827e797204
Amira-S-Abdelaziz <e.amira67@gmail.com>
2023-08-03 17:37:05 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - function
+ * @s: var
+ * Return: smthing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		_putchar(*(s + i));
+		i++;
+	}
+	_putchar('\n');
+}
